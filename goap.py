aa8e7f7e29eba1409684620c68875721243b44f8
Josh <joshuac@tyemill.com>
2015-10-18 23:26:47 -0700

    Rewrite find_actions_leading_to_goal to fix issue where multiple preconditions for behavior were being executed as alternate paths.

--- goap.py
+++ goap.py
@@ -43,8 +43,6 @@ class TestEntity:
 
 
 
-
-
 class AtLocation:
     def __init__(self, initial_location, target_location, entity):
         self.initial_location = initial_location
@@ -71,6 +69,8 @@ class IsHangingOut:
         # behavior gets analyzed
         return (self.entity.wx, self.entity.wy) == self.target_location
 
+    def get_name(self):
+        return 'spend time at {0}'.format(g.WORLD.tiles[self.target_location[0]][self.target_location[1]].get_location_description())
 
 class GoodsAreUnloaded:
     def __init__(self, target_city, goods, entity):
@@ -83,6 +83,9 @@ class GoodsAreUnloaded:
     def is_completed(self):
             return self.entity in self.target_city.caravans
 
+    def get_name(self):
+        return 'have goods unloaded in {0}'.format(self.target_city.name)
+
 class GoodsAreLoaded:
     def __init__(self, target_city, goods, entity):
         self.target_city = target_city
@@ -94,6 +97,8 @@ class GoodsAreLoaded:
     def is_completed(self):
             return self.entity in self.target_city.caravans
 
+    def get_name(self):
+        return 'have goods in {0} loaded'.format(self.target_city.name)
 
 class HaveCommodityAtLocation:
     def __init__(self, commodity, quantity, entity, target_location):
@@ -107,14 +112,10 @@ class HaveCommodityAtLocation:
 
 
     def is_completed(self):
-        has_all_commodities = True
-        for commodity, quantity in self.commodities_and_quantities.iteritems():
-            if (commodity not in self.entity.creature.econ_inventory) or self.entity.creature.econ_inventory[self.commodity] < quantity:
-                has_all_commodities = False
-                break
+        return self.entity.creature.econ_inventory[self.commodity] >= self.quantity and (self.entity.wx, self.entity.wy) == self.target_location
 
-        return has_all_commodities and (self.entity.wx, self.entity.wy) == self.target_location
-
+    def get_name(self):
+        return 'have {0} {1} at {2}'.format(self.quantity, self.commodity, g.WORLD.tiles[self.target_location[0]][self.target_location[1]].get_location_description())
 
 class HaveCommodity:
     ''' State of having a commodity in inventory, and potentially being at a particular location once the commodity is owned '''
@@ -135,11 +136,15 @@ class HaveCommodity:
         elif data.commodity_manager.name_is_good(self.commodity):
             self.behaviors_to_accomplish.append(DoReaction(self.commodity, self.quantity, self.entity))
 
+        else:
+            g.game.add_message('Input is not commodity', libtcod.red)
 
     def is_completed(self):
         ''' Target location is optional to include, so only check for it if necessary '''
         return self.entity.creature.econ_inventory[self.commodity] >= self.quantity
 
+    def get_name(self):
+        return 'have {0} {1}'.format(self.quantity, self.commodity)
 
 class HaveItem:
     def __init__(self, item_name, entity):
@@ -151,47 +156,48 @@ class HaveItem:
     def is_completed(self):
         return self.item_name in self.entity.creature.possessions
 
+    def get_name(self):
+        return 'have {0}'.format(self.item_name)
 
-class KnowWhereItemisLocated:
-    def __init__(self, item, entity):
-        self.item = item
-        self.entity = entity
-        self.behaviors_to_accomplish = [FindOutWhereItemIsLocated(self.item, self.entity)]
-
-    def is_completed(self):
-        return self.entity.creature.knowledge['objects'][self.item]['location']['accuracy'] == 1
-
-
-class HaveRoughIdeaOfLocation:
-    def __init__(self, item, entity):
-        self.item = item
-        self.entity = entity
-        self.behaviors_to_accomplish = []
-
-    def is_completed(self):
-        return self.entity.creature.knowledge['objects'][self.item]['location']['accuracy'] <= 2
-
-
-class HaveMoney:
-    def __init__(self, money, entity):
-        self.money = money
-        self.entity = entity
-        # Will be set if this status isn't already completed
-        self.behaviors_to_accomplish = [GetMoneyThroughWork(self.money, self.entity), StealMoney(self.money, self.entity)]
-
-    def is_completed(self):
-        return 1
-        #return self.entity.creature.gold >= self.money
-
-
-class HaveJob:
-    def __init__(self, entity):
-        self.entity = entity
-        # Will be set if this status isn't already completed
-        self.behaviors_to_accomplish = [GetJob(self.entity)]
-
-    def is_completed(self):
-        return self.entity.creature.profession
+# class KnowWhereItemisLocated:
+#     def __init__(self, item, entity):
+#         self.item = item
+#         self.entity = entity
+#         self.behaviors_to_accomplish = [FindOutWhereItemIsLocated(self.item, self.entity)]
+#
+#     def is_completed(self):
+#         return self.entity.creature.knowledge['objects'][self.item]['location']['accuracy'] == 1
+#
+#
+# class HaveRoughIdeaOfLocation:
+#     def __init__(self, item, entity):
+#         self.item = item
+#         self.entity = entity
+#         self.behaviors_to_accomplish = []
+#
+#     def is_completed(self):
+#         return self.entity.creature.knowledge['objects'][self.item]['location']['accuracy'] <= 2
+#
+# class HaveMoney:
+#     def __init__(self, money, entity):
+#         self.money = money
+#         self.entity = entity
+#         # Will be set if this status isn't already completed
+#         self.behaviors_to_accomplish = [GetMoneyThroughWork(self.money, self.entity), StealMoney(self.money, self.entity)]
+#
+#     def is_completed(self):
+#         return 1
+#         #return self.entity.creature.gold >= self.money
+#
+#
+# class HaveJob:
+#     def __init__(self, entity):
+#         self.entity = entity
+#         # Will be set if this status isn't already completed
+#         self.behaviors_to_accomplish = [GetJob(self.entity)]
+#
+#     def is_completed(self):
+#         return self.entity.creature.profession
 
 
 class AmAvailableToAct:
@@ -203,6 +209,8 @@ class AmAvailableToAct:
     def is_completed(self):
         return self.entity.creature.is_available_to_act()
 
+    def get_name(self):
+        return 'be available to act'
 
 
 class ActionBase:
@@ -403,13 +411,23 @@ class DoReaction(ActionBase):
 
         # Store this reaction
         self.reaction = data.commodity_manager.reactions[commodity]
+        self.consumed_in_this_reaction = {}
         self.number_of_reactions = int(ceil(quantity / self.reaction.output_amount))
+        assert self.number_of_reactions > 0, self.number_of_reactions
 
         # Amount we need total for the reacion, accounting for the amount input / output quantities
         input_quantity =  self.number_of_reactions * self.reaction.input_amount
         self.preconditions = [HaveCommodity(commodity=self.reaction.input_commodity_name, quantity=input_quantity, entity=entity)]
 
-        for commodity_type, quantity in chain(self.reaction.commodities_consumed.iteritems(), self.reaction.commodities_required.iteritems()):
+        ## For consumed items, we mut have enough to fuel the entire reaction
+        for commodity_type, quantity in self.reaction.commodities_consumed.iteritems():
+            commodity = random.choice(data.commodity_manager.get_names_of_commodities_of_type(commodity_type=commodity_type))
+            quantity_needed_for_this_goal = quantity * self.number_of_reactions
+            self.consumed_in_this_reaction[commodity] = quantity_needed_for_this_goal
+            self.preconditions.append(HaveCommodity(commodity=commodity, quantity=quantity_needed_for_this_goal, entity=entity))
+
+        ## For required items, just having the # specified in the yaml is sufficient, and these do not get consumed in the reaction
+        for commodity_type, quantity in self.reaction.commodities_required.iteritems():
             commodity = random.choice(data.commodity_manager.get_names_of_commodities_of_type(commodity_type=commodity_type))
             self.preconditions.append(HaveCommodity(commodity=commodity, quantity=quantity, entity=entity))
 
@@ -434,8 +452,18 @@ class DoReaction(ActionBase):
         ''' Increment progress counter, and do reaction if we've toiled long enough '''
         self.behavior_progress += 1
         if self.behavior_progress >= self.days_of_reaction:
+
             self.entity.creature.econ_inventory[self.commodity] += (self.number_of_reactions * self.reaction.output_amount)
+
             self.entity.creature.econ_inventory[self.reaction.input_commodity_name] -= (self.number_of_reactions * self.reaction.input_amount)
+            assert self.entity.creature.econ_inventory[self.reaction.input_commodity_name] >= 0, \
+                                                        '{0}\'s inventory of {1} was {2} after doing reaction'.format(self.entity.fulltitle(),
+                                                        self.reaction.input_commodity_name, self.entity.creature.econ_inventory[self.reaction.input_commodity_name])
+
+            for commodity, quantity_needed_for_this_goal in self.consumed_in_this_reaction.iteritems():
+                self.entity.creature.econ_inventory[commodity] -= quantity_needed_for_this_goal
+                assert self.entity.creature.econ_inventory[commodity] >= 0, '{0}\'s inventory of {1} was {2} after doing reaction'.format(self.entity.fulltitle(),
+                                                                            commodity, self.entity.creature.econ_inventory[commodity])
 
             #g.game.add_message('{0} has created {1} {2} (originally needed {3}'.format(self.entity.fulltitle(), self.number_of_reactions * self.reaction.output_amount, self.commodity, self.quantity), libtcod.red)
             #g.game.add_message(' - {0}: {1}, {2}: {3}'.format(self.commodity, self.entity.creature.econ_inventory[self.commodity],
@@ -516,30 +544,88 @@ class LoadGoodsBehavior(ActionBase):
 
 
 
+def find_actions_leading_to_goals(goal_states, action_path, all_possible_paths):
+    ''' Recursive function to find all possible behaviors which can be undertaken to get to a particular goal. This
+    function will drill down, checking behavior options and goal states, and return all valid paths to that goal that it can find.
 
+    :param goal_states: <list> containing <any of the GoalStates defined in this module> - The state of the world that the goal seeker would like to change
+    :param action_path: <list> contianing the current sequence of <ActionBase> behaviors to accomplish the goal - since the function is recursive, it grows on each call
+    :param all_possible_paths: <list> containing sequences of <ActionBase> behaviors which have reached a valid conclusion (e.g. they have drilled down to states with no unmet conditions)
+    :return: a <list> containing <lists> of behavior sequences which have reached a valid conclusion
+    '''
+    # TODO - using pop(0) is inefficient, consider using deques rather than lists throughout the function
 
-def find_actions_leading_to_goal(goal_state, action_path, all_possible_paths):
-    ''' Recursive function to find all possible behaviors which can be undertaken to get to a particular goal '''
-    #print ' --- ', r_level, goal_state.status, [a.behavior for a in action_list], ' --- '
+    # Pop the first goal in the stack to evaluate
+    goal_state = goal_states.pop(0)
 
+    # Loop through each behavior option. These are different possibilities for how a goal can be completed, meaning
+    # there may be multiple valid paths an agent can take to reach a goal
     for behavior_option in goal_state.behaviors_to_accomplish:
-        unmet_conditions = behavior_option.get_unmet_conditions()
-        current_action_path = [behavior_option] + action_path # Copy of the new behavior + action_path
+        # Find goal states which need to be accomplished for the behavior to be valid. Then add the current set of goal
+        # states to the end of this list - ensuring that these sub-goals get evaluated first
+        unmet_goal_states = behavior_option.get_unmet_conditions()
+        unmet_goal_states.extend(goal_states)
 
-        # If there are conditions that need to be met, then we find the actions that can be taken to complete each of them
-        for condition in unmet_conditions:
-            find_actions_leading_to_goal(goal_state=condition, action_path=current_action_path, all_possible_paths=all_possible_paths)
+        # The new action path is the old action path with the new one inserted at the beginning
+        current_action_path = [behavior_option] + action_path  # TODO - this is making a copy of the list each time - evaluate if needed
 
-        # If all conditions are met, then this behavior can be accomplished, so it gets added to the list
-        if not unmet_conditions:
+        # If there are unment goal states (either as prerequisites to this behavior, or "left over" from previous calls
+        # of this function, recursively call this function to find valid behavior paths.
+        if unmet_goal_states:
+            find_actions_leading_to_goals(goal_states=unmet_goal_states, action_path=current_action_path, all_possible_paths=all_possible_paths)
+
+        # If all conditions are met, then this behavior can be accomplished, so the current set of behavior paths get
+        # added to the master list of valid behavior paths to reach this goal
+        elif not unmet_goal_states:
             all_possible_paths.append(current_action_path)
-            #print [a.behavior for a in current_action_path]
-            #all_paths_worked = adjust_path_for_movement(entity=test_entity, initial_location=(goal_state.entity.wx, goal_state.entity.wy), action_path=current_action_path, all_paths_worked=[])
-            #for i, p in enumerate(all_paths_worked):
-                # print i, [a.behavior for a in p]
-                #all_possible_paths.append(p)
 
     return all_possible_paths
+
+
+
+# def find_actions_leading_to_goal(goal_state, other_goals, action_path, all_possible_paths):
+#     # Loop through each behavior option. These are different possibilities
+#     for behavior_option in goal_state.behaviors_to_accomplish:
+#         unmet_conditions = behavior_option.get_unmet_conditions()
+#         current_action_path = [behavior_option] + action_path # Copy of the new behavior + action_path
+#
+#         if unmet_conditions:
+#             main_goal = unmet_conditions.pop(0)
+#             find_actions_leading_to_goal(goal_state=main_goal, other_goals=unmet_conditions, action_path=current_action_path, all_possible_paths=all_possible_paths)
+#
+#         # If there are other goals, we can pop that into the goal slot
+#         elif (not unmet_conditions) and other_goals:
+#             main_goal = other_goals.pop(0)
+#             find_actions_leading_to_goal(goal_state=main_goal, other_goals=other_goals, action_path=current_action_path, all_possible_paths=all_possible_paths)
+#
+#         # If all conditions are met, then this behavior can be accomplished, so it gets added to the list
+#         elif (not unmet_conditions) and (not other_goals):
+#             all_possible_paths.append(current_action_path)
+#
+#     return all_possible_paths
+
+
+# def find_actions_leading_to_goal(goal_state, action_path, all_possible_paths):
+#     ''' Recursive function to find all possible behaviors which can be undertaken to get to a particular goal '''
+#     #print ' --- ', r_level, goal_state.status, [a.behavior for a in action_list], ' --- '
+#
+#     for behavior_option in goal_state.behaviors_to_accomplish:
+#         unmet_conditions = behavior_option.get_unmet_conditions()
+#         current_action_path = [behavior_option] + action_path # Copy of the new behavior + action_path
+#
+#         # If there are conditions that need to be met, then we find the actions that can be taken to complete each of them
+#         for condition in unmet_conditions:
+#             # print condition.get_name()
+#             test = find_actions_leading_to_goal(goal_state=condition, action_path=current_action_path, all_possible_paths=all_possible_paths)
+#             # for b_list in test:
+#             #     print [b.get_name() for b in b_list]
+#             # print ''
+#
+#         # If all conditions are met, then this behavior can be accomplished, so it gets added to the list
+#         if not unmet_conditions:
+#             all_possible_paths.append(current_action_path)
+#
+#     return all_possible_paths
 
 
 def check_paths_for_movement(entity, behavior_lists):
@@ -587,9 +673,18 @@ def get_behavior_list_costs(behavior_lists):
 def get_costed_behavior_paths(goal_state, entity):
     ''' Find a set of behavior paths to get to a goal, insert any required movements, cost the resulting behavior lists, and return the costed lists '''
     # Find possible actions that can be taken to get there
-    all_possible_behavior_paths = find_actions_leading_to_goal(goal_state=goal_state, action_path=[], all_possible_paths=[])
+    all_possible_behavior_paths = find_actions_leading_to_goals(goal_states=[goal_state], action_path=[], all_possible_paths=[])
+
     # Account for movement (eventually, account for movement subtrees?)
     behavior_list_including_movement = check_paths_for_movement(entity=entity, behavior_lists=all_possible_behavior_paths)
+
+    ## DEBUG PRINT GOAL PATHS
+    # if 'load' not in goal_state.get_name():
+    #     print goal_state.get_name()
+    #     for path in behavior_list_including_movement:
+    #         print [b.get_name() for b in path]
+    #     print ''
+
     # With movement now factored in, get costs
     behavior_lists_costed = get_behavior_list_costs(behavior_lists=behavior_list_including_movement)
 
